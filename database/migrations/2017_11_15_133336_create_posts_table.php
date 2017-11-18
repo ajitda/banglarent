604d14b81799ce6fc4b6a919409e95c62e6d8fd9
ajitda <ajitdas2900@gmail.com>
2017-11-18 22:53:27 +0600

    user and post relation added

--- database/migrations/2017_11_15_133336_create_posts_table.php
+++ database/migrations/2017_11_15_133336_create_posts_table.php
@@ -21,7 +21,8 @@ class CreatePostsTable extends Migration
             $table->string('image');
             $table->text('address');
             $table->text('description');
-
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
