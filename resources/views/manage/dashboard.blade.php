47bdbd1ab30a503ff01dcb57ff8b7b4a09538597
ajitda <ajitdas2900@gmail.com>
2017-12-18 23:50:47 +0600

    user and post manage page

--- resources/views/manage/dashboard.blade.php
+++ resources/views/manage/dashboard.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.manage')
 
 @section('content')
-
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h1>All the Users</h1>
+		</div>
+		<div class="panel-body">
+			
+		</div>
+	</div>
 @endsection
